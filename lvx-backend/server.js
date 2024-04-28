565045759420157a0df2c930f00cf0f366a6685d
VanshBishnoi <121486071+VanshBishnoi@users.noreply.github.com>
2024-04-28 17:54:11 +0530

    Update server.js

--- lvx-backend/server.js
+++ lvx-backend/server.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./index");
-dotenv.config({ path: "./.env" });
 const DB=process.env.DB
 mongoose
-  .connect(`mongodb+srv://vanshbishnoi:vansh@cluster0.itgtevb.mongodb.net/`, {
+  .connect(process.env.DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
