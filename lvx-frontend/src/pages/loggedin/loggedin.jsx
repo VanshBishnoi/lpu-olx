415a36233abedb36ee63d77523038ef1caf9f922
VanshBishnoi <121486071+VanshBishnoi@users.noreply.github.com>
2024-04-28 17:42:28 +0530

    Update loggedin.jsx

--- lvx-frontend/src/pages/loggedin/loggedin.jsx
+++ lvx-frontend/src/pages/loggedin/loggedin.jsx
@@ -11,26 +11,11 @@ function Loggedin() {
     window.location.reload()
   }
   return (
-    <>
+    <div id='logout'>
     <button className='logout' onClick={handleLogout}>Logout</button>
     <div>Hello</div>
-    </>
+    </div>
   )
 }
 
 export default Loggedin;
-
-// import React from "react";
-// import { useAuth0 } from "@auth0/auth0-react";
-
-// const Loggedin = () => {
-//   const { logout } = useAuth0();
-
-//   return (
-//     <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-//       Log Out
-//     </button>
-//   );
-// };
-
-// export default Loggedin;
